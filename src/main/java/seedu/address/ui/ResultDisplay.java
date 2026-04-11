f188b81b1bf864c82a869bb8187c0d5e30f302ab
Manya <e1459283@u.nus.edu>
2026-04-12 00:23:49 +0800

    Wrap long display message to the next line

--- src/main/java/seedu/address/ui/ResultDisplay.java
+++ src/main/java/seedu/address/ui/ResultDisplay.java
@@ -5,6 +5,7 @@ import static java.util.Objects.requireNonNull;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 import javafx.scene.layout.Region;
+import javafx.scene.layout.StackPane;
 
 /**
  * A ui for the status bar that is displayed at the header of the application.
@@ -18,6 +19,8 @@ public class ResultDisplay extends UiPart<Region> {
 
     public ResultDisplay() {
         super(FXML);
+        StackPane root = (StackPane) getRoot();
+        resultDisplay.maxWidthProperty().bind(root.widthProperty());
     }
 
     public void setFeedbackToUser(String feedbackToUser) {
